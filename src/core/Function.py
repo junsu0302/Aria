5f9c73ff4c9dc32a68223e643340445822224398
junsu0302 <junsu96378224@gmail.com>
2024-04-10 14:52:18 +0900

    #1 - 역전파 구현
    함수가 연쇄적으로 진행되어도 반복문을 통해 최종 기울기를 산출하는 역전파 구현
    
    순전파를 진행할 때 모든 Variable은 자신을 파생시킨 부모 함수를 저장한다. 그리고 모든 Function은 입출력 값을 저장하여 역전파를 진행할 때 참조한다.

--- src/core/Function.py
+++ src/core/Function.py
@@ -5,7 +5,13 @@ class Function:
     x = input.data # 데이터 로드
     y = self.forward(x) # 순전파 계산
     output = Variable(y) # 계산 결과
+    output.set_creator(self) # 부모 함수 설정
+    self.input = input # 입력 값 저장
+    self.output = output # 출력 값 저장
     return output
   
   def forward(self, x):
+    raise NotImplementedError()
+  
+  def backward(self, gy):
     raise NotImplementedError()
--- src/core/Variable.py
+++ src/core/Variable.py
@@ -1,3 +1,23 @@
+import numpy as np
+
 class Variable:
   def __init__(self, data):
-    self.data = data # 데이터 저장
+    self.data = data # 데이터
+    self.grad = None # 기울기
+    self.creator = None # 부모 함수
+
+  def set_creator(self, func):
+    self.creator = func
+
+  def backward(self):
+    if self.grad is None:
+      self.grad = np.ones_like(self.data)
+
+    funcs = [self.creator]
+    while funcs:
+      f = funcs.pop() # 함수 획득
+      x, y = f.input, f.output # 함수 입출력 획득
+      x.grad = f.backward(y.grad) # 역전파 진행
+
+      if x.creator is not None:
+        funcs.append(x.creator)
