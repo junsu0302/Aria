b139ef323024d55ca6b961f07440b88f88da354f
junsu0302 <junsu96378224@gmail.com>
2024-04-17 21:16:43 +0900

    #3 - Model 설정 및 MLP 구현
    신경망 구조를 설정할 수 있는 Model 클래스를 정의한다.
    
    Model 클래스는 Layer 클래스를 상속받아 Layer가 중첩되어 만들어지는 형식으로 구성했다.
    
    신경망 구조의 경우 복잡해지면 CLI로는 구조 파악이 힘들기 때문에 plot()을 이용해 시각화가 가능하게 구성했다.
    
    Model을 상속받아 TwoLayerNet을 만들어 테스트를 진행했다.
    
    테스트가 성공하고 다층으로 모델을 구성할 수 있는 MLP를 구현했다.

---
+++ Aria/model.py
@@ -0,0 +1,37 @@
+from Aria.layer import Layer
+from Aria.utils.Visualize import plot_dot_graph
+
+import Aria.layer as L
+from Aria.activation import sigmoid
+
+class Model(Layer):
+  def plot(self, *inputs, to_file='model.png'):
+    y = self.forward(*inputs)
+    return plot_dot_graph(y, verbose=True, to_file=to_file)
+  
+class TwoLayerNet(Model):
+  def __init__(self, hidden_size, out_size):
+    super().__init__()
+    self.l1 = L.Linear(hidden_size)
+    self.l2 = L.Linear(out_size)
+
+  def forward(self, x):
+    y = sigmoid(self.l1(x))
+    y = self.l2(y)
+    return y
+  
+class MLP(Model):
+  def __init__(self, fc_output_sizes, activation=sigmoid):
+    super().__init__()
+    self.activation = activation
+    self.layers = []
+
+    for i, out_size in enumerate(fc_output_sizes):
+      layer = L.Linear(out_size)
+      setattr(self, 'l' + str(i), layer)
+      self.layers.append(layer)
+
+  def forward(self, x):
+    for l in self.layers[:-1]:
+      x = self.activation(l(x))
+    return self.layers[-1](x)
